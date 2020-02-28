956204a11d14ef503589fc0f2bdc151635d55415
Mikael Rozee <mikaelsrozee@gmail.com>
2020-02-28 20:15:59 +0000

    setup mod files

---
+++ src/main/java/codes/msr/drops/Dictionary.java
@@ -0,0 +1,9 @@
+package codes.msr.drops;
+
+public class Dictionary {
+
+    public static final String MOD_ID = "drops";
+    public static final String MOD_NAME = "Drops";
+    public static final String VERSION = "1.12.2-0.0.0.0";
+
+}
---
+++ src/main/java/codes/msr/drops/Drops.java
@@ -0,0 +1,7 @@
+package codes.msr.drops;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Dictionary.MOD_ID, name = Dictionary.MOD_NAME, version = Dictionary.VERSION)
+public class Drops {
+}
