dc8a70fa85f360eb08e6af6cf5e7859ead9fd737
Mikael Rozee <mikaelsrozee@gmail.com>
2020-03-01 16:26:55 +0000

    spawn beacon to show drop location

--- src/main/java/codes/msr/drops/server/DropEventHandler.java
+++ src/main/java/codes/msr/drops/server/DropEventHandler.java
@@ -1,5 +1,7 @@
 package codes.msr.drops.server;
 
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -26,6 +28,20 @@ public class DropEventHandler {
     public void endWorldTick(TickEvent.WorldTickEvent event) {
         if (DropHandler.INSTANCE != null) {
             DropHandler.INSTANCE.endWorldTick(event);
+        }
+    }
+
+    @SubscribeEvent
+    public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (!event.getWorld().isRemote && event.getWorld().provider.getDimension() == 0) {
+            DropHandler.INSTANCE.onRightClickBlock(event);
+        }
+    }
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (!event.getWorld().isRemote && event.getWorld().provider.getDimension() == 0) {
+            DropHandler.INSTANCE.onBlockBreak(event);
         }
     }
 
--- src/main/java/codes/msr/drops/server/DropHandler.java
+++ src/main/java/codes/msr/drops/server/DropHandler.java
@@ -1,12 +1,15 @@
 package codes.msr.drops.server;
 
 import codes.msr.drops.common.config.ConfigHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -14,6 +17,8 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.storage.WorldSavedData;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import javax.annotation.Nonnull;
@@ -23,7 +28,7 @@ public class DropHandler extends WorldSavedData {
 
     public static DropHandler INSTANCE;
 
-    boolean forced;
+    boolean forced, dropActive;
     int dayCounter;
     BlockPos dropPos;
 
@@ -37,6 +42,17 @@ public class DropHandler extends WorldSavedData {
             if (world.provider.getDimension() == 0) {
                 int time = (int) (world.getWorldTime() % 24000);
 
+                if (dropActive && dropPos != null && world.getWorldTime() % 40 == 0) {
+                    spawnBeam();
+                }
+
+                if (dropActive && dropPos != null && time == 0) {
+                    alertDrop(world, dropPos, "despawned", TextFormatting.DARK_RED);
+                    world.removeTileEntity(dropPos);
+                    world.setBlockToAir(dropPos);
+                    dropActive = false;
+                }
+
                 if (time == 13000) {
                     dayCounter++;
                     this.markDirty();
@@ -46,20 +62,50 @@ public class DropHandler extends WorldSavedData {
                     if (forced) {
                         dropPos = findDropPosition(world);
                         spawnDrop(world, dropPos);
-                        alertDrop(world, dropPos, true);
+                        alertDrop(world, dropPos, "spawningNow", TextFormatting.GOLD);
+                        dropActive = true;
                         forced = false;
                     } else if (time == 13000) {
                         dropPos = findDropPosition(world);
-                        alertDrop(world, dropPos, false);
+                        alertDrop(world, dropPos, "spawning", TextFormatting.RED);
                     } else if (time == 18000 && dropPos != null) {
                         spawnDrop(world, dropPos);
-                        alertDrop(world, dropPos, true);
+                        alertDrop(world, dropPos, "spawningNow", TextFormatting.GOLD);
 
                         dayCounter = 0;
+                        dropActive = true;
                         this.markDirty();
                     }
                 }
             }
+        }
+    }
+
+    private void spawnBeam() {
+        Minecraft mc = Minecraft.getMinecraft();
+        WorldClient world = mc.world;
+
+        for (int y = dropPos.getY() + 1; y < world.getActualHeight(); y++) {
+            EnumParticleTypes particle = y % 2 == 0 ? EnumParticleTypes.EXPLOSION_NORMAL : EnumParticleTypes.REDSTONE;
+
+            double x = dropPos.getX() + 0.5D + (world.rand.nextDouble() - 0.25D);
+            double z = dropPos.getZ() + 0.5D + (world.rand.nextDouble() - 0.25D);
+
+            world.spawnParticle(particle, x, y, z, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (dropActive && event.getPos().equals(dropPos)) {
+            dropActive = false;
+            alertDrop(event.getWorld(), dropPos, "captured", TextFormatting.BLUE);
+        }
+    }
+
+    public void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (dropActive && event.getPos().equals(dropPos)) {
+            dropActive = false;
+            alertDrop(event.getWorld(), dropPos, "captured", TextFormatting.BLUE);
         }
     }
 
@@ -137,9 +183,8 @@ public class DropHandler extends WorldSavedData {
         }
     }
 
-    private void alertDrop(World world, BlockPos pos, boolean spawning) {
-        String message = spawning ? "text.drops.spawningNow" : "text.drops.spawning";
-        TextFormatting colour = spawning ? TextFormatting.RED : TextFormatting.GOLD;
+    private void alertDrop(World world, BlockPos pos, String alert, TextFormatting colour) {
+        String message = "text.drops." + alert;
 
         for (Object object : world.playerEntities) {
             EntityPlayer player = (EntityPlayer) object;
@@ -160,6 +205,7 @@ public class DropHandler extends WorldSavedData {
     }
 
     @Override
+    @Nonnull
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         nbt.setInteger("dayCounter", dayCounter);
 
